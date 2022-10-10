7fbab4d734a6f5bcfe4f7528f2d399d675f9d175
jannowicki12 <95223624+jannowicki12@users.noreply.github.com>
2022-10-10 11:44:20 +0200

    a

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
+from .models import Question
+from django.shortcuts import render, get_object_or_404
 
-# Create your views here.
+def question_list(request):
+   questions = Question.objects.all()
+   return render(request, 'polls/question_list.html', {'questions': 
+questions})
+
+def question_detail(request, question_id):
+   question = get_object_or_404(Question, pk=question_id)
+   return render(request, 'polls/question_detail.html', {'question': question})
