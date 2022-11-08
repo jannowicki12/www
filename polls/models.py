c409e03b0cc94df6b79450bf71a17ed24cf24975
jannowicki12 <95223624+jannowicki12@users.noreply.github.com>
2022-11-08 11:05:31 +0100

    4.2

--- polls/models.py
+++ polls/models.py
@@ -1,20 +1,24 @@
 from django.db import models
 
-
 class Question(models.Model):
+    question_text = models.CharField(max_length=200, null=True)       
+    pub_date = models.DateTimeField("date published")
+
     def __str__(self):
         return self.question_text
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
-    is_open = models.BooleanField(default=True)
 
-
-
-# catpuccin
-class Choice(models.Model):
+class Answer(models.Model):   
     question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer = models.TextField(max_length=200)
+    
+    def __str__(self):
+        return self.answer
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, null=True)
     choice_text = models.CharField(max_length=200)
     votes = models.IntegerField(default=0)
+    answer = models.ForeignKey(Answer, on_delete=models.CASCADE)
 
-class Answer(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    def __str__(self):
+        return self.choice_text
--- polls/views.py
+++ polls/views.py
@@ -2,10 +2,11 @@ from .models import Question
 from django.shortcuts import render, get_object_or_404
 
 def question_list(request):
-   questions = Question.objects.all()
+   questions = Question.objects.all().order_by('-question_text')
    return render(request, 'polls/question_list.html', {'questions': 
 questions})
 
 def question_detail(request, question_id):
-   question = get_object_or_404(Question, pk=question_id)
+   # question = get_object_or_404(Question, pk=question_id)
+   question = Question.objects.filter(pk=question_id)
    return render(request, 'polls/question_detail.html', {'question': question})
